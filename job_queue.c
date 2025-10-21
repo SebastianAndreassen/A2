6836bad448b637958bec9f3aace2afe1281a69cf
SebastianAndreassen <bwj836@alumni.ku.dk>
2025-10-21 15:23:27 +0200

    pop og destroy

--- job_queue.c
+++ job_queue.c
@@ -32,32 +32,51 @@ int job_queue_destroy(struct job_queue *job_queue) {
 
 int job_queue_push(struct job_queue *job_queue, void *data) {
   // lock the lock to protect elements with shared state e.g. head, tail and buffer
-    pthread_mutex_lock(&job_queue->lock);
+  pthread_mutex_lock(&job_queue->lock);
 
-    // pushing threads are blocked while the queue is full
-    while (job_queue->count == job_queue->capacity && !job_queue->destroying) {
-        pthread_cond_wait(&job_queue->not_full, &job_queue->lock);
-    }
+  // pushing threads are blocked while the queue is full
+  while (job_queue->count == job_queue->capacity && !job_queue->destroying) {
+      pthread_cond_wait(&job_queue->not_full, &job_queue->lock);
+  }
 
-    // cannot push to a queue being destroyed
-    if (job_queue->destroying) {
-        pthread_mutex_unlock(&job_queue->lock);
-        return -1;
-    }
+  // cannot push to a queue being destroyed
+  if (job_queue->destroying) {
+      pthread_mutex_unlock(&job_queue->lock);
+      return -1;
+  }
 
-    // data is pushed to the tail which then advances
-    job_queue->buffer[job_queue->tail] = data;
-    job_queue->tail = (job_queue->tail + 1) % job_queue->capacity;
-    job_queue->count++;
+  // data is pushed to the tail which then advances
+  job_queue->buffer[job_queue->tail] = data;
+  job_queue->tail = (job_queue->tail + 1) % job_queue->capacity;
+  job_queue->count++;
 
-    // wake threads that are blocked from popping due empty queue
-    pthread_cond_signal(&job_queue->not_empty);
+  // wake threads that are blocked from popping due empty queue
+  pthread_cond_signal(&job_queue->not_empty);
 
-    // release lock after altering shared state elements
-    pthread_mutex_unlock(&job_queue->lock);
-    return 0;
+  // release lock after altering shared state elements
+  pthread_mutex_unlock(&job_queue->lock);
+  return 0;
 }
 
 int job_queue_pop(struct job_queue *job_queue, void **data) {
-  assert(0);
+  pthread_mutex_lock(&job_queue->lock);
+
+  while (count == 0 && !job_queue->destroying) {
+        pthread_cond_wait(&job_queue->not_empty, &job_queue->lock);
+    }
+
+  if (job_queue->destroying && count == 0) {
+    pthread_mutex_unlock(&lock);
+    return -1;
+  }
+
+  *data = job_queue->buffer[job_queue->head];
+  job_queue->head = (job_queue->head + 1) % job_queue->capacity;
+  job_queue->count--;
+
+  pthread_cond_signal(&job_queue->not_full);
+
+  pthread_mutex_unlock(&job_queue->lock);
+  return 0;
+
 }
